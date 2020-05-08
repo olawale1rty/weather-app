a1a6a4b216ba7bb42759ec78ddca242c7839cac5
Mustapha Moshood <olawale3415@gmail.com>
2020-05-08 13:56:07 +0100

    weathertrack

---
+++ weather_app/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=159216a33869ab33d86ac04063cc8a70'
+    if request.method == 'POST': # only true if form is submitted
+        form = CityForm(request.POST) # add actual request data to form for processing
+        form.save() # will validate and save if validate
+
+    cities = City.objects.all() #return all the cities in the database
+    form = CityForm()
+    weather_data = []
+
+    for city in cities:
+
+        city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types
+
+        weather = {
+            'city' : city,
+            'temperature' : city_weather['main']['temp'],
+            'description' : city_weather['weather'][0]['description'],
+            'icon' : city_weather['weather'][0]['icon'],
+            'long' : city_weather['coord']['lon'],
+            'lat' : city_weather['coord']['lat'],
+            'country' : city_weather['sys']['country'],
+
+        }
+
+        weather_data.append(weather) #add the data for the current city into our list
+
+    context = {'weather_data' : weather_data, 'form' : form}
+
+    return render(request, 'weather_app/index.html', context) #returns the index.html template
